f4521c320f4fb6b09ef36d5b4e82c1453316ed9a
starindustries05@gmail.com <starindustries05@gmail.com>
2022-01-01 22:11:35 +0530

    pvc deletion not implemented

--- commands/PVC/create.js
+++ commands/PVC/create.js
@@ -2,6 +2,8 @@ const pvcsys = require('../../pvcsystem')
 const economy = require('../../economy')
 const {Message,MessageActionRow,MessageButton, Collector, ButtonInteraction} = require("discord.js")
 
+cache = []
+
 module.exports = {
     aliases: ['br'],
     category: 'PVC',
@@ -29,7 +31,7 @@ module.exports = {
         let matches = text.match(/\d+/g);
         let letters = text.match(/\D+/g) || [];
         let time = 0;
-        console.log({ matches, letters });
+        // console.log({ matches, letters });
         if ((new Set(letters)).size !== letters.length) console.log("Error"); // Checking for duplicates
         for (let i = 0; i < letters.length; i++) { // Converting in seconds
             if (letters[i][0] == 'h') time += matches[i] * 60 * 60;
@@ -38,7 +40,8 @@ module.exports = {
         }
         if (matches.length - 1 == letters.length) time += matches[letters.length] * 1;
         // console.log({time});
-        if(time<60) console.log("Min time error");
+        if(time<60) {await message.reply("Create a PVC of atlest 1 min. ||Dont be a CHEAPDA||") ;
+        return};
         let currentDate = message.createdTimestamp.toString().slice(0,-3);
         // console.log({currentDate})
 
@@ -70,16 +73,23 @@ module.exports = {
                 .setLabel("Cancel")
                 .setStyle("DANGER")
         )
-        message.channel.send({content:`This trans would cost you ${cost}`, components: [row]})
+        if(cache.includes(message.author.id)){
+            errMsg = await message.reply("You have a pending transaction above please wait atleast 30s or respond to the previous message.");
+            
+            return;
+        }
+        message.reply({content:`This trans would cost you ${cost}`, components: [row]})
+        cache.push(message.author.id)
 
         filter = (interaction) =>{
             if (interaction.user.id === message.author.id){return true}
             return interaction.reply({content:"Not your button to click", ephemeral:true})
         }
 
-         const collector = message.channel.createMessageComponentCollector({filter,max:1})
+         const collector = message.channel.createMessageComponentCollector({filter,max:1,time:30000})
 
         await collector.on("end",async (ButtonInteraction) => {
+            cache = cache.filter((value,index,array) => value != message.author.id)
             let btnreply = ButtonInteraction.first().customId
             console.log(btnreply)
             if (btnreply=="reject")
@@ -87,7 +97,7 @@ module.exports = {
             await ButtonInteraction.first().reply({content:"Transaction Processed"})
 
             const {categId} = await pvcsys.findCateg({guildId})
-            await economy.addCoins({guildId,userId,coins:-1*cost})
+
             
 
             let parent = await message.guild.channels.fetch(categId)
@@ -97,7 +107,7 @@ module.exports = {
             await textChannel.permissionOverwrites.edit(message.author.id,{"ADD_REACTIONS":true ,"VIEW_CHANNEL":true,"SEND_MESSAGES":true,"READ_MESSAGE_HISTORY":true,"CREATE_INSTANT_INVITE":true,"EMBED_LINKS":true,"ATTACH_FILES":true,"USE_EXTERNAL_EMOJIS":true},{reason:"PVC created",type:1})
             await voiceChannel.edit({userLimit:1})
             await textChannel.permissionOverwrites.edit(message.author.id,{"STREAM":true,"VIEW_CHANNEL":true,"CONNECT":true,"SPEAK":true},{reason:"PVC created",type:1})
-
+            await economy.addCoins({guildId,userId,coins:-1*cost})
             
             // timeRem = endDate - currentDate
             // endDate = endDate.toString()
